78dc9efcac8f7dc243d9bc240b307c6b38d5b91e
Adeel Arif Bhatti <adeel.bhatti@huddly.com>
2023-12-23 14:30:21 +0100

    Type languages is added (struct) Slice of the languages is created and four members are added. The router/HandleFunc and the start-server/ListenAndServe are moved to a separate function.

--- main.go
+++ main.go
@@ -1,12 +1,38 @@
 package main
 import ("fmt"
 		"net/http"
+		"log"
+		"encoding/json"
+		"reflect"
+		"strconv"
 		)
+type Language struct {
+	name string
+	id int
+}
+var languages []Language
+
 func mainpage(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w,"Welcome")
-	fmt.Println("Docker changes")
+	log.Println("Docker changes")
+}
+func returnLanguages(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	log.Println("returnLanguages")
+	json.NewEncoder(w).Encode(languages)
+}
+func startServer(){
+	http.HandleFunc("/",mainpage)
+	http.HandleFunc("/languages",returnLanguages)
+	http.ListenAndServe(":8080",nil)
 }
 func main(){
-	http.HandleFunc("/",mainpage)
-	http.ListenAndServe(":8080",nil)
+	languages = []Language{
+		Language{name:"Golang", id: 1},
+		Language{name:"Java", id: 2},
+		Language{name:"Python", id: 3},
+		Language{name:"Kotin", id: 4},
+	}
+	startServer()
 }
