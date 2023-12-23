afae866cb2ca6815c2dbf14be5feac5e408c3508
Adeel Arif Bhatti <adeel.bhatti@huddly.com>
2023-12-23 16:36:00 +0100

    Added the  returnLanguage for the single language.

--- main.go
+++ main.go
@@ -17,14 +17,41 @@ func mainpage(w http.ResponseWriter, r *http.Request){
 	log.Println("Docker changes")
 }
 func returnLanguages(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
 	log.Println("returnLanguages")
 	json.NewEncoder(w).Encode(languages)
+}
+func returnLanguage(w http.ResponseWriter, r *http.Request){
+	log.Println("returnLanguage")
+	log.Println(r.URL.Path)
+	key := r.URL.Path[len("/language/"):]
+	log.Println("key is ",key)
+	for _, languages := range languages {
+		// log.Println("Type of key is: \n", reflect.TypeOf(key))
+		id,err := strconv.Atoi(key)
+		log.Println("err in strconv.Atoi(languages.id) is ", err," id is ", reflect.TypeOf(id))
+		// log.Println("Type of key is: \n", reflect.TypeOf(key))
+		if languages.id == id {
+			fmt.Println("languages.id ",languages.id)
+			fmt.Println("languages.name ",languages.name)
+			fmt.Println(" languages.id ", languages.id)
+			json.NewEncoder(w).Encode(languages.id)
+			fmt.Println("languages.name ", languages.name)
+			fmt.Println("language json response  ",languages)
+			json.NewEncoder(w).Encode(languages.name)
+			// w.Header().Set("Content-Type", "application/json")
+			// w.WriteHeader(http.StatusCreated)
+			json.NewEncoder(w).Encode(languages)
+			fmt.Println("languages.id ",languages.id)
+		}
+	}
+	// log.Println(languages)
+	// log.Println(json.NewEncoder(w).Encode(languages))
+	// json.NewEncoder(w).Encode(languages)
 }
 func startServer(){
 	http.HandleFunc("/",mainpage)
 	http.HandleFunc("/languages",returnLanguages)
+	http.HandleFunc("/language/",returnLanguage)
 	http.ListenAndServe(":8080",nil)
 }
 func main(){
