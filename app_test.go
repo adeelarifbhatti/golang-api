b3a63875bc6b63e9e048b0629e836f0d03c1c827
Adeel Arif Bhatti <adeel.bhatti@huddly.com>
2024-01-05 07:15:34 +0100

    First test for testing the GET single language from /language/1

--- app_test.go
+++ app_test.go
@@ -4,19 +4,23 @@ import(
 	"log"
 	"testing"
 	"fmt"
+	"net/http/httptest"
+	"net/http"
 )
 var a App
 
-func  TestMain(m *testing.M) {
-	a.Start("root",DbPassword,DBName)
+func TestMain(m *testing.M) {
+	err := a.Start("root",DbPassword,DBName)
+	if err != nil {
+		log.Fatal(err)
+	}
 	createDB()
-	createLanguage("Julie")
 	m.Run()
 }
 func createDB() {
 	createDatabase := `CREATE DATABASE testing;`
 	useDatabase := `use testing;`
-	createTable := `CREATE TABLE testing(id int Primary Key NOT NULL AUTO_INCREMENT,name VARCHAR(255));`
+	createTable := `CREATE TABLE languages(id int Primary Key NOT NULL AUTO_INCREMENT,name VARCHAR(255));`
 	_, err := a.DB.Exec(createDatabase)
 	if err != nil {
 		log.Fatal(err)
@@ -30,11 +34,36 @@ func createDB() {
 		log.Fatal(err)
 	}
 }
-func createLanguage(name string){
-	query := fmt.Sprintf("insert into testing(name) values('%v')",name)
-	_, err := a.DB.Exec(query)
+func clearTable() {
+	a.DB.Exec("delete from testing")
+	a.DB.Exec("Alter table testing Auto_Increment=1")
+}
+func addLanguage(name string){
+	clearTable()
+	query := fmt.Sprintf("insert into languages(name) values('%v')", name)
+	_,err := a.DB.Exec(query)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+func TestGetLanguage(t *testing.T) {
+	clearTable()
+	addLanguage("Java")
+	req, _ := http.NewRequest("GET","/language/1",nil)
+	response := sendRequest(req)
+	checkStatusCode(t, http.StatusOK,response.Code)
+}
 
+func checkStatusCode(t *testing.T, expect int, gotten int){
+	if expect != gotten {
+		t.Errorf("Expected %v, Got %v", expect,gotten)
+	}
+	if expect == gotten {
+	fmt.Println("expected was ### ", expect, " and what we got is ### " ,gotten)
+	}
+}
+func sendRequest(req *http.Request) *httptest.ResponseRecorder {
+	recorder := httptest.NewRecorder()
+	a.Router.ServeHTTP(recorder, req)
+	return recorder
 }
